b871f92233bdafafc6b98734ab7626737cce5701
taherkhan30 <taherkhan@live.co.uk>
2023-02-20 18:08:43 +0000

    changed pl and aks.sh

--- aks.sh
+++ aks.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 set -x
-az account set --subscription $1
-aks_resource_group=$2
-aks_name=$3
-department=$4
+az account set --subscription 'Pay-As-You-Go'
+aks_resource_group='rg-mycluster'
+aks_name='mycluster'
+department='cft'
 {
 printf "\n\nTrying cluster $aks_name $aks_resource_group\n"
 az aks get-credentials \
