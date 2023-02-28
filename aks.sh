f056148134f762852dada947a94ff1e2279f8246
taherkhan30 <taherkhan@live.co.uk>
2023-02-28 09:25:07 +0000

    test 4

--- aks.sh
+++ aks.sh
@@ -1,16 +1,17 @@
 #! /usr/bin/env bash
 set -x
 az account set --subscription 'Pay-As-You-Go'
-aks_resource_group='rg-mycluster'
-aks_name='mycluster'
+aks_resource_group='rg-cluster'
+aks_name='my-cluster'
 department='cft'
 {
 printf "\n\nTrying cluster $aks_name $aks_resource_group\n"
 az aks get-credentials \
     --resource-group $aks_resource_group \
     --name $aks_name
-./pluto
-./pluto detect-helm
+output=`./pluto detect-helm`
+echo $output
+export output
 }
 
 
