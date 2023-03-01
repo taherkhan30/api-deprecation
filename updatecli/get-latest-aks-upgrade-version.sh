4d8a4e656684f5cccb896a2208c17b572c064cf7
taherkhan30 <taherkhan@live.co.uk>
2023-03-01 23:29:10 +0000

    testing git workflow

--- updatecli/get-latest-aks-upgrade-version.sh
+++ updatecli/get-latest-aks-upgrade-version.sh
@@ -11,26 +11,26 @@ get_upgrades () {
       --name "${aks_name}" \
       --resource-group "${aks_resource_group}" \
       --subscription "${aks_subscription}" 
-    #   | jq -r '
-    #   if (.controlPlaneProfile.upgrades|map(select(.isPreview == null)) == []) then
-    #       .controlPlaneProfile.kubernetesVersion
-    #   else .controlPlaneProfile.upgrades|map(select(.isPreview == null).kubernetesVersion)
-    #   | .[] end' \
-    #   | sort --reverse --version-sort | head --lines 1
+      | jq -r '
+      if (.controlPlaneProfile.upgrades|map(select(.isPreview == null)) == []) then
+          .controlPlaneProfile.kubernetesVersion
+      else .controlPlaneProfile.upgrades|map(select(.isPreview == null).kubernetesVersion)
+      | .[] end' \
+      | sort --reverse --version-sort | head --lines 1
 
 }
 
 UPGRADE_VERSION=$(get_upgrades)
 
-# # For environments usually only running one cluster at a time
-# # check the second cluster if response from 00 is empty
-# if [[ ${UPGRADE_VERSION} == "" ]] && [[ ${environment} =~ ^(preview|demo)$ ]]; then
+# For environments usually only running one cluster at a time
+# check the second cluster if response from 00 is empty
+if [[ ${UPGRADE_VERSION} == "" ]] && [[ ${environment} =~ ^(preview|demo)$ ]]; then
 
-#   aks_name=$(echo "${aks_name}" | sed -E "s/00/01/g" )
-#   aks_resource_group=$(echo "${aks_resource_group}" | sed -E "s/00/01/g" )
+  aks_name=$(echo "${aks_name}" | sed -E "s/00/01/g" )
+  aks_resource_group=$(echo "${aks_resource_group}" | sed -E "s/00/01/g" )
 
-#   UPGRADE_VERSION=$(get_upgrades)
+  UPGRADE_VERSION=$(get_upgrades)
 
-# fi
+fi
 
 echo "${UPGRADE_VERSION}"
