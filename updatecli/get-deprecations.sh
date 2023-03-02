e5037cbe9b748dcd41424497179fe7ebf38081e4
taherkhan30 <taherkhan@live.co.uk>
2023-03-02 16:43:03 +0000

    new day 18

--- updatecli/get-deprecations.sh
+++ updatecli/get-deprecations.sh
@@ -1,5 +1,5 @@
  #!/bin/bash
-       
+
 set -x
 az account set --subscription 'Pay-As-You-Go'
 aks_resource_group='rg-cluster'
@@ -7,7 +7,7 @@ aks_name='my-cluster'
 environment=sbox
 
 # get pluto output 
-get-deprecations () {
+get_deprecations () {
     az aks get-credentials \
         --resource-group "${aks_resource_group}" \
         --name "${aks_name}" \
