6ab8fb3f9fe73fb106665409bfe403d49f4783b9
taherkhan30 <taherkhan@live.co.uk>
2023-03-02 15:48:00 +0000

    new day 18

--- updatecli/get-deprecations.sh
+++ updatecli/get-deprecations.sh
@@ -3,7 +3,7 @@ set -x
 az account set --subscription 'Pay-As-You-Go'
 aks_resource_group='rg-cluster'
 aks_name='my-cluster'
-department='cft'
+environment=sbox
 
 # get pluto output 
 get-deprecations () {
@@ -16,27 +16,4 @@ get-deprecations () {
 }
 
 DEPRECATIONS=$(get_deprecations)
-
-# #! /usr/bin/env bash
-# set -x
-# az account set --subscription $1
-# aks_resource_group=$2
-# aks_name=$3
-# environment=$4
-# department=$5
-# {
-# printf "\n\nTrying cluster $aks_name $aks_resource_group\n"
-# az aks get-credentials \
-#     --resource-group $aks_resource_group \
-#     --name $aks_name --admin
-# # Return false and fallback to cluster 01 if cluster returns 0 ingress objects
-# $(kubectl get ingress --context "$aks_name-admin" --all-namespaces=true -o \
-#     go-template='{{ $length := len .items }} {{ if eq $length 0 }}false{{ end }}')
-# } || {
-# aks_resource_group=$(echo $aks_resource_group|sed 's/-00-/-01-/g')
-# aks_name=$(echo $aks_name|sed 's/-00-/-01-/g')
-# printf "\n\nTrying cluster $aks_name $aks_resource_group\n"
-# az aks get-credentials \
-#     --resource-group $aks_resource_group \
-#     --name $aks_name --admin
-# }
+echo "${DEPRECATIONS}"
