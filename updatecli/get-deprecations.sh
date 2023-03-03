0cfb3fc76efd3f456d969421ccf8781d1d4ee7f6
taherkhan30 <taherkhan@live.co.uk>
2023-03-03 22:32:09 +0000

    14

--- updatecli/get-deprecations.sh
+++ updatecli/get-deprecations.sh
@@ -1,9 +1,9 @@
  #!/bin/bash
 
-{{ $environment := (requiredEnv "CURRENT_ITER_ENVIRONMENT") }}
-{{ $aks_name := (index .environments $environment).aks_name }}
-{{ $aks_resource_group := (index .environments $environment).aks_resource_group }}
-{{ $aks_subscription := (index .environments $environment).aks_subscription }}
+$environment := (requiredEnv "CURRENT_ITER_ENVIRONMENT")
+$aks_name := (index .environments $environment).aks_name
+$aks_resource_group := (index .environments $environment).aks_resource_group
+$aks_subscription := (index .environments $environment).aks_subscription
 
 # aks_name=$1
 # aks_resource_group=$2
