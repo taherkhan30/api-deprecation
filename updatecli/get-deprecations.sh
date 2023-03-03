c0b25fe30900c35fd15922ea806584287c2a08b0
taherkhan30 <taherkhan@live.co.uk>
2023-03-03 22:05:53 +0000

    friday 12

--- updatecli/get-deprecations.sh
+++ updatecli/get-deprecations.sh
@@ -1,10 +1,8 @@
  #!/bin/bash
-
-set -x
-az account set --subscription 'Pay-As-You-Go'
-aks_resource_group='rg-cluster'
-aks_name='my-cluster'
-environment=sbox
+aks_name=$1
+aks_resource_group=$2
+aks_subscription=$3
+environment=$4
 
 az aks get-credentials --resource-group "${aks_resource_group}" --name "${aks_name}" --admin
 pluto detect-helm 
