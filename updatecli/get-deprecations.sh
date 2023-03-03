9dd2b17464138d276331440755225066f900b3d3
taherkhan30 <taherkhan@live.co.uk>
2023-03-03 23:07:47 +0000

    14

--- updatecli/get-deprecations.sh
+++ updatecli/get-deprecations.sh
@@ -1,20 +1,20 @@
  #!/bin/bash
 
-$environment := (requiredEnv "CURRENT_ITER_ENVIRONMENT")
-$aks_name := (index .environments $environment).aks_name
-$aks_resource_group := (index .environments $environment).aks_resource_group
-$aks_subscription := (index .environments $environment).aks_subscription
+# $environment := (requiredEnv "CURRENT_ITER_ENVIRONMENT")
+# $aks_name := (index .environments $environment).aks_name
+# $aks_resource_group := (index .environments $environment).aks_resource_group
+# $aks_subscription := (index .environments $environment).aks_subscription
 
 # aks_name=$1
 # aks_resource_group=$2
 # aks_subscription=$3
 # environment=$4
 
-# set -x
-# az account set --subscription 'Pay-As-You-Go'
-# aks_resource_group='rg-cluster'
-# aks_name='my-cluster'
-# environment=sbox
+set -x
+az account set --subscription 'Pay-As-You-Go'
+aks_resource_group='rg-cluster'
+aks_name='my-cluster'
+environment=sbox
 
 az aks get-credentials --resource-group "${aks_resource_group}" --name "${aks_name}" --admin
 pluto detect-helm 
--- updatecli/get-latest-aks-upgrade-version.sh
+++ updatecli/get-latest-aks-upgrade-version.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-aks_name=my-cluster
-aks_resource_group=rg-cluster
-aks_subscription=Pay-As-You-Go
-environment=sbox
+aks_name=$1
+aks_resource_group=$2
+aks_subscription=$3
+environment=$4
 
 get_upgrades () {
   az aks get-upgrades \
