ee50cf5647892291928a9e416de991722cb99cc8
taherkhan30 <taherkhan@live.co.uk>
2023-03-05 15:14:59 +0000

    sunday 3

--- updatecli/get-deprecations.sh
+++ updatecli/get-deprecations.sh
@@ -11,6 +11,7 @@ NAME=sam; echo "$CURRENT_ITER_ENVIRONMENT"
 NAME=sam && echo "$CURRENT_ITER_ENVIRONMENT"
 echo "$CURRENT_ITER_ENVIRONMENT"
 aks_name=`yq -r '.environments[].aks_name' ./values.github-action.yaml`
+aks_resource_group=`yq -r '.environments[].aks_resource_group' ./values.github-action.yaml`
 
 
 # $aks_name := (index .environments $environment).aks_name
