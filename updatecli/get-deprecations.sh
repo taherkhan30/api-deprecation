ff608289b87a8fff0308e93e4f6ca5cb041da719
taherkhan30 <taherkhan@live.co.uk>
2023-03-05 14:28:32 +0000

    sunday 1

--- updatecli/get-deprecations.sh
+++ updatecli/get-deprecations.sh
@@ -1,4 +1,13 @@
- #!/bin/bash
+#!/usr/bin/env bash
+
+{{ $environment := (requiredEnv "CURRENT_ITER_ENVIRONMENT") }}
+{{ $aks_name := (index .environments $environment).aks_name }}
+{{ $aks_resource_group := (index .environments $environment).aks_resource_group }}
+{{ $aks_subscription := (index .environments $environment).aks_subscription }}
+
+az aks get-credentials --resource-group "$aks_resource_group" --name "$aks_name" --admin
+
+pluto detect helm
 
 # $environment := (requiredEnv "CURRENT_ITER_ENVIRONMENT")
 # $aks_name := (index .environments $environment).aks_name
@@ -46,12 +55,3 @@
 # -   https://learn.microsoft.com/en-us/azure/devops/pipelines/ecosystems/github-actions?view=azure-devops
 # - update pr using azure devops task 
 # - 
-
-{{ $environment := (requiredEnv "CURRENT_ITER_ENVIRONMENT") }}
-{{ $aks_name := (index .environments $environment).aks_name }}
-{{ $aks_resource_group := (index .environments $environment).aks_resource_group }}
-{{ $aks_subscription := (index .environments $environment).aks_subscription }}
-
-az aks get-credentials --resource-group "$aks_resource_group" --name "$aks_name" --admin
-
-pluto detect helm
