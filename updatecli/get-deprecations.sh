a60e90eef650e5442bdf36a6818a49ebd60327d5
taherkhan30 <taherkhan@live.co.uk>
2023-03-05 15:08:03 +0000

    sunday 3

--- updatecli/get-deprecations.sh
+++ updatecli/get-deprecations.sh
@@ -9,7 +9,8 @@
 # $environment := requiredEnv "CURRENT_ITER_ENVIRONMENT"
 NAME=sam; echo "$CURRENT_ITER_ENVIRONMENT"
 NAME=sam && echo "$CURRENT_ITER_ENVIRONMENT"
-aks_name='$(yq -r '.environments[].aks_name' ./values.github-action.yaml'
+echo "$CURRENT_ITER_ENVIRONMENT"
+aks_name=`yq -r '.environments[].aks_name' ./values.github-action.yaml`
 
 
 # $aks_name := (index .environments $environment).aks_name
@@ -18,7 +19,7 @@ aks_name='$(yq -r '.environments[].aks_name' ./values.github-action.yaml'
 
 az aks get-credentials --resource-group "$aks_resource_group" --name "$aks_name" --admin
 
-pluto detect helm
+pluto detect-helm
 
 # $environment := (requiredEnv "CURRENT_ITER_ENVIRONMENT")
 # $aks_name := (index .environments $environment).aks_name
@@ -28,12 +29,17 @@ pluto detect helm
 # aks_resource_group=$2
 # aks_subscription=$3
 # environment=$4
+
 # yq -r '.environments.sbox | to_entries[].value | split(":")[0]' values.github-action.yaml
+
 # yq -r '.environments | to_entries | map(select(.value.enabled == true)) | .[].key' ./values.github-action.yaml
+
 # yq -r '.environments.sbox | to_entries | map(select(.value.enabled == true)) | .[].key' ./values.github-action.yaml
-# yq "$environments,sbox" ./values.github-action.yaml
+
 # yq -r '.environments.sbox' ./values.github-action.yaml
+
 # aks_name=$(yq -r '.environments[].aks_name' ./values.github-action.yaml
+
 # aks_rg_array=(aks_resource_group=$(yq -r '.environments[].aks_resource_group' ./values.github-action.yaml))
 # aks_subscription=$(yq -r '.environments[].aks_subscription' ./values.github-action.yaml)
 # aks_rg_array=(aks_aks_resource_group=$(yq -r '.environments[].aks_resource_group' ./values.github-action.yaml))
